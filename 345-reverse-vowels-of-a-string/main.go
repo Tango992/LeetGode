c1e5728beb11e9c19acdffd7a0887afef142a335
Tango992 <daniel.rahmanto@gmail.com>
2023-12-09 22:46:10 +0700

    progress

--- 345-reverse-vowels-of-a-string/main.go
+++ 345-reverse-vowels-of-a-string/main.go
@@ -13,13 +13,13 @@ func main() {
 
 func reverseVowels(s string) string {
 	var (
-		vowels = "aiueoAIUEO"
+		vowels = "aiueo"
 		vowelsReversed, finalString string
 		vowelsCounter int
 	)
 	
 	for _, c := range s {
-		if strings.ContainsRune(vowels, c) {
+		if strings.ContainsAny(strings.ToLower(string(s)), vowels) {
 			vowelsReversed = string(c) + vowelsReversed
 			continue
 		}
