6f8c28d6ad94b5a21d2384efb42867318c501589
Tango992 <daniel.rahmanto@gmail.com>
2024-01-30 20:56:17 +0700

    valid anagram

---
+++ 242-valid-anagram/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := isAnagram("anagram", "nagaram")
+	fmt.Println(result)
+}
+
+func isAnagram(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	var freq [26]int
+
+	for idx := 0; idx < len(s); idx++ {
+		freq[s[idx]-'a']++
+		freq[t[idx]-'a']--
+	}
+
+	for idx := 0; idx < len(freq); idx++ {
+		if freq[idx] != 0 {
+			return false
+		}
+	}
+
+	return true
+}
