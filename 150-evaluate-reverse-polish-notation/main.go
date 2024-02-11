0f10f031596429a910f313c81e8cdc63c0fd442a
Tango992 <daniel.rahmanto@gmail.com>
2024-02-11 17:37:14 +0700

    implements stack using array

--- 150-evaluate-reverse-polish-notation/main.go
+++ 150-evaluate-reverse-polish-notation/main.go
@@ -6,15 +6,40 @@ import (
 )
 
 func main() {
-	res := evalRPN([]string{"10","6","9","3","+","-11","","/","","17","+","5","+"})
+	res := evalRPN2([]string{"10","6","9","3","+","-11","*","/","*","17","+","5","+"})
 	fmt.Println(res)
 }
+
+func evalRPN2(tokens []string) int {
+	ops := map[string]func(int, int) int{
+		"+": func(i, j int) int {return i+j},
+		"-": func(i, j int) int {return i-j},
+		"*": func(i, j int) int {return i*j},
+		"/": func(i, j int) int {return i/j},
+	}
+	
+	firstNum, _ := strconv.Atoi(tokens[0])
+	stack := []int{firstNum}
+	
+	for _, val := range tokens[1:] {
+		num, err := strconv.Atoi(val)
+		if err != nil {
+			calculatedValue := ops[val](stack[len(stack)-2], stack[len(stack)-1])
+			stack = append(stack[:len(stack)-2], calculatedValue)
+			continue
+		}
+		stack = append(stack, num)
+	}
+	return stack[0]
+}
+
 
 type Stack struct {
 	value int
 	next  *Stack
 }
 
+// Alternative using Stack struct
 func evalRPN(tokens []string) int {
 	ops := map[string]func(int, int) int{
 		"+": func(i, j int) int {return i+j},
